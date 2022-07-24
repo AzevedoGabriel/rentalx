d304afa3090601b5bb6ac12329cd65f52e82e00d
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2022-07-24 10:54:50 -0300

    criada classe catregoy

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,21 @@
 /* eslint-disable import/prefer-default-export */
 import { Router } from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidv4(),
+  const category = new Category();
+
+  Object.assign(category, {
     name,
     description,
-  };
+    created_at: new Date(),
+  });
 
   categories.push(category);
 
