d0152206b3175ea802496312ac4e120a878de582
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2022-07-24 08:57:27 -0300

    inserindo id com uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,21 @@
 /* eslint-disable import/prefer-default-export */
 import { Router } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
+  const category = {
+    id: uuidv4(),
     name,
     description,
-  });
+  };
+
+  categories.push(category);
 
   return response.status(201).send();
 });
