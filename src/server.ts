0e9870ccf75cf1134498a0504273e3da077cada2
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2022-07-24 08:24:13 -0300

    debugger ajusterd

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-const express = require('express')
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World Ignite!' });
+});
+
+app.post('/course', (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
+});
+
+app.listen(3333, () => console.log('Server is running!'));
