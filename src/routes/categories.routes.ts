a1eb5595f8400baab442cd8f10781484163d652e
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2022-07-24 11:20:15 -0300

    ajueste completos na rota de categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,16 @@
 /* eslint-disable import/prefer-default-export */
 import { Router } from 'express';
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  return response.status(201).json({ category });
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
 });
 
 export { categoriesRoutes };
