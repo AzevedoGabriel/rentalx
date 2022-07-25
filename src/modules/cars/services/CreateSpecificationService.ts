b68b23bda5d918a3da757c300f05f1fd572287c6
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2022-07-25 08:08:43 -0300

    rota e criar de especificações concluído

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,7 +1,24 @@
-class CreateSepecificationService {
-  execute() {
-    console.log('TODO');
+import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
+import { ISpecificationRepository } from '../repositories/ISpecificationRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  constructor(private specificationRepository: ISpecificationRepository) {}
+
+  execute({ description, name }: IRequest) {
+    const specificationAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error('Specification Already Exists!');
+    }
+
+    this.specificationRepository.create({ name, description });
   }
 }
 
-export { CreateSepecificationService };
+export { CreateSpecificationService };
