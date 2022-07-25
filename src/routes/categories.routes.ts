8571966c53d15f51c80b3356147982486114b048
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2022-07-25 15:16:20 -0300

    importando categoria por aquivo csv

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,16 @@
 /* eslint-disable import/prefer-default-export */
 import { Router } from 'express';
+import multer from 'multer';
+
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesControler } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
@@ -11,6 +18,10 @@ categoriesRoutes.post('/', (request, response) => {
 
 categoriesRoutes.get('/', (request, response) => {
   return listCategoriesControler.handle(request, response);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  return importCategoryController.handle(request, response);s
 });
 
 export { categoriesRoutes };
