608ffa81107573d1ff9ffd6a7537535a4459d5a3
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2022-07-25 15:33:10 -0300

    read line csv

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,19 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable import/prefer-default-export */
+import { parse as csvParse } from 'csv-parse';
+import fs from 'fs';
+
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on('data', async line => {
+      console.log(line);
+    });
   }
 }
 
